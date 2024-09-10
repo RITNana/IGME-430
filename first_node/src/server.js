b74fcadb70a30b287ee8c398ee261cc7be22ffbf
RITNana <ns8363@g.rit.edu>
2024-09-10 19:33:20 -0400

    first node

---
+++ first_node/src/server.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+const port = 3000
+
+const onRequest = (req, res) => {
+    console.log(req.url)
+    res.writeHeader(200,{'Content-Type': 'text/plain'})
+    res.write(`Hello Server - you added for ${res.url}`)
+    res.end()
+}
+
+http.createServer(onRequest).listen(port,  () => {
+    // init callback
+    console.log(`Server running on ${port}`)
+})
